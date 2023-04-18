18014956bccd0184ab590ad1d3d7381ccf6033cd
deconvolution <deconvolution@unr.edu>
2023-04-17 18:11:39 -0700

    add continuous mode for plot pathway network

--- R/plot-pathway-network.R
+++ R/plot-pathway-network.R
@@ -6,24 +6,69 @@
 #' Plot a pathway network
 #' @description This function plots a pathway network.
 #' @param results A named list of data frame of Pathway analysis results.
-#' The columns of each data frame should be at least ID, name, p.value and pFDR, ES, NES, and nDE.
+#' The columns of each data frame should be at least ID, name, p.value and pFDR and nDE.
+#' An optional column "color" can be used to specify the color of the nodes.
+#' If the column "color" is not specified, the color of the nodes will be determined by the mode and the statistic.
 #' @param genesets A named list of character vectors of gene sets.
+#' @param statistic A character value of the statistic to use for the network.
+#' The statistic should be one of the columns of the data frame in the results.
+#' @param mode A character value of the mode to use to color the nodes.
+#' The mode should be one of "discrete", "continous".
+#' If the mode is "discrete", the color of the nodes are determined by whether the p-value is significant or not.
+#' If the mode is "continous", the color of the nodes are proportional to the statistic.
 #' @param labels A named character vector of labels for the results.
 #' @param pThreshold A numeric value of p-value threshold.
 #' @param useFDR A logical value indicating whether to use FDR or not.
+#' This parameter is independent of the pThreshold.
 #' @param edgeThreshold A numeric from 0 to 1 indicating the threshold to draw edges.
 #' edgeThreshold of 0.1 means that edges are drawn if the number of genes in common is greater than 1% of the smaller gene set.
+#' @param statLimit A numeric value of the maximum absolute value of the statistic.
+#' If statistic is p.value or pFDR, this parameter is the limit of -log10(p-value).
+#' @param discreteColors A character vector of colors to use for the discrete mode.
+#' The length of the vector must be the same as the number of results.
+#' @param continuousScaleFunc A function that takes a numeric value from -1 to 1 and returns a color.
+#' @param NAColor A character value of the color to use for NA values.
+#' @param borderColor A character value of the color to use for the border of the nodes.
+#' @param edgeColor A character value of the color to use for the edges.
+#' @param nodeSizeFnc A function that takes a character value of the ID of the node and returns a numeric value of the size of the node.
+#' @param borderWidthFnc A function that takes a character value of the ID of the node and returns a numeric value of the width of the border of the node.
+#' @param edgeWidthFnc A function that takes a character value of the ID of the from node and a character value of the ID of the to node and returns a numeric value of the width of the edge.
 #' @param styleFile A character value of the path to the style file.
 #' If NULL, the default style file will be used, which is located at system.file(package="RCPA", "extdata", "pieStyle.js")
 #' @return A RCyjs object.
+#' @details
+#' The function will plot a pathway network using the results of pathway analysis.
+#' The nodes of the network are the pathways and the edges are the pathways that have at least a certain number of genes in common defined by the edgeThreshold.
+#' The size of the nodes are proportional to the number of genes in the pathway.
+#' The color of the nodes are proportional to the statistic used if the mode is "continous".
+#' If the mode is "discrete", the color of the nodes are determined by whether the p-value is significant or not.
+#' The width of the edges are proportional to the number of genes in common.
 #' @export
 #' @examples
 #' #TODO add examples
-#' @importFrom RCyjs setGraph redraw layout loadStyleFile
+#' @importFrom RCyjs setGraph redraw layout loadStyleFile setDefaultEdgeColor
 #' @importFrom BrowserViz BrowserViz
 #' @importFrom graph graphNEL addEdge nodeDataDefaults nodeData
 #' @importFrom dplyr filter mutate %>%
-plotPathwayNetwork <- function(results, genesets, labels = NULL, pThreshold = 0.05, useFDR = TRUE, edgeThreshold = 0.1, styleFile = NULL) {
+plotPathwayNetwork <- function(results, genesets,
+                               statistic = "pFDR",
+                               mode = c("continuous", "discrete"),
+                               labels = NULL,
+                               pThreshold = 0.05,
+                               useFDR = TRUE,
+                               edgeThreshold = 0.1,
+                               statLimit = 4,
+                               discreteColors = NULL,
+                               continuousScaleFunc = NULL,
+                               NAColor = "#dddddd",
+                               borderColor = "#333333",
+                               edgeColor = "#dddddd",
+                               nodeSizeFnc = function(id) length(genesets[[id]])^.75,
+                               borderWidthFnc = function(id) 1,
+                               edgeWidthFnc = function(from, to) length(intersect(genesets[[from]], genesets[[to]]))^.5/2,
+                               styleFile = NULL) {
+
+    mode <- match.arg(mode)
 
     cyjsQueryFnc <- function(queryString)
     {
@@ -49,28 +94,90 @@ plotPathwayNetwork <- function(results, genesets, labels = NULL, pThreshold = 0.
         styleFile <- system.file(package = "RCPA", "extdata", "pieStyle.js")
     }
 
+    for (res in results) {
+        if (!all(c("ID", "name", "p.value", "pFDR", "nDE") %in% colnames(res))) {
+            stop("The columns of the data frame in the results should be at least ID, name, p.value, pFDR, and nDE.")
+        }
+
+        if (!statistic %in% colnames(res)) {
+            stop("The statistic should be one of the columns of the data frame in the results.")
+        }
+    }
+
+    if (is.null(discreteColors)) {
+        discretePieColors <- c(
+            "#316b9d",
+            "#fce397",
+            "#99cc83",
+            "#f77a65",
+            "#a6a1d0",
+            "#fea9c4",
+            "#74e7bc",
+            "#febb73",
+            "#1db4db",
+            "#ffc5a6",
+            "#b6c9fa",
+            "#ee5437"
+        )
+    } else if (mode == "discrete") {
+        if (length(discreteColors) != length(results)) {
+            stop("The length of the discreteColors should be the same as the number of results.")
+        }
+    }
+
+    if (is.null(continuousScaleFunc)) {
+
+        continuousScaleFunc <- function(x) {
+            x <- ifelse(x >= 1, 0.999999, x)
+            x <- ifelse(x <= -1, -0.999999, x)
+
+            colorRampPalette(c("blue", "white", "red"))(1000)[ceiling((x + 1) / 2 * 1000)]
+        }
+
+    }
+
     pathwayInfo <- data.frame(
         ID = names(genesets),
         label = if (!is.null(labels)) labels else names(genesets),
-        size = sapply(names(genesets), function(x) length(genesets[[x]])),
+        size = sapply(names(genesets), nodeSizeFnc),
+        borderWidth = sapply(names(genesets), borderWidthFnc),
+        borderColor = borderColor,
         nResult = length(results)
     )
 
     for (i in seq_along(results)) {
-        pathwayInfo[[paste0("pvalue", i)]] <- NA
-        pathwayInfo[[paste0("isSig", i)]] <- NA
-        pathwayInfo[[paste0("nDE", i)]] <- NA
+
+        pathwayInfo[[paste0("stat", i)]] <- NA
+        pathwayInfo[[paste0("isSig", i)]] <- FALSE
+        # pathwayInfo[[paste0("nDE", i)]] <- NA
+
+        statValues <- results[[i]][[statistic]]
+        if (statistic == "p.value" || statistic == "pFDR") {
+            statValues <- -log10(statValues)
+            statValues[statValues > statLimit] <- statLimit
+            statValues <- statValues / statLimit
+        } else {
+            statValues[statValues > statLimit] <- statLimit
+            statValues[statValues < -statLimit] <- -statLimit
+            statValues <- statValues / statLimit
+        }
 
         idx <- match(pathwayInfo$ID, results[[i]]$ID)
 
-        pathwayInfo[[paste0("nDE", i)]] <- results[[i]]$nDE[idx]
+        pathwayInfo[[paste0("stat", i)]] <- statValues[idx]
+        # pathwayInfo[[paste0("nDE", i)]] <- results[[i]]$nDE[idx]
 
         if (useFDR) {
-            pathwayInfo[[paste0("pvalue", i)]] <- results[[i]]$pFDR[idx]
             pathwayInfo[[paste0("isSig", i)]] <- results[[i]]$pFDR[idx] < pThreshold
         } else {
-            pathwayInfo[[paste0("pvalue", i)]] <- results[[i]]$p.value[idx]
             pathwayInfo[[paste0("isSig", i)]] <- results[[i]]$p.value[idx] < pThreshold
+        }
+
+        if (mode == "discrete") {
+            pathwayInfo[[paste0("color", i)]] <- ifelse(pathwayInfo[[paste0("isSig", i)]], discretePieColors[i], NAColor)
+        } else {
+            pathwayInfo[[paste0("color", i)]] <- continuousScaleFunc(pathwayInfo[[paste0("stat", i)]])
+            pathwayInfo[[paste0("color", i)]][is.na(pathwayInfo[[paste0("stat", i)]])] <- NAColor
         }
     }
 
@@ -82,16 +189,17 @@ plotPathwayNetwork <- function(results, genesets, labels = NULL, pThreshold = 0.
                 to = gsIDs[j],
                 from.size = length(genesets[[i]]),
                 to.size = length(genesets[[j]]),
-                nCommon = length(intersect(genesets[[i]], genesets[[j]])),
+                common = length(intersect(genesets[[i]], genesets[[j]])),
                 stringsAsFactors = FALSE
             )
         }) %>% do.call(what = rbind)
     }) %>%
         do.call(what = rbind) %>%
-        filter(.$nCommon / min(.$from.size, .$to.size) > edgeThreshold) %>%
-        mutate(
-            weight = nCommon
-        )
+        filter(.$common / min(.$from.size, .$to.size) > edgeThreshold)
+
+    graphEdges$weight <- apply(graphEdges[, c("from", "to")], 1, function(x) {
+        edgeWidthFnc(x[1], x[2])
+    })
 
     graphObj <- graphNEL(pathwayInfo$ID, edgemode = "undirected")
     graphObj <- graph::addEdge(graphEdges$from, graphEdges$to, graphObj, graphEdges$weight)
@@ -104,12 +212,13 @@ plotPathwayNetwork <- function(results, genesets, labels = NULL, pThreshold = 0.
     rCy <- BrowserViz(portRange = 10000:20000,
                       title = "Pathway network",
                       quiet = T,
-                      browserFile = system.file(package="RCyjs", "browserCode", "dist", "rcyjs.html"),
+                      browserFile = system.file(package = "RCyjs", "browserCode", "dist", "rcyjs.html"),
                       httpQueryProcessingFunction = cyjsQueryFnc,
     ) %>% .RCyjs(graph = graphObj)
 
     RCyjs::setGraph(rCy, graph = graphObj)
     RCyjs::loadStyleFile(rCy, styleFile)
+    RCyjs::setDefaultEdgeColor(rCy, edgeColor)
     RCyjs::redraw(rCy)
     Sys.sleep(2)
     RCyjs::layout(rCy, "cose")
--- tests/testthat/test-plot-pathway-network.R
+++ tests/testthat/test-plot-pathway-network.R
@@ -2,6 +2,8 @@ library(testthat)
 library(RCyjs)
 library(RCPA)
 
+devtools::load_all()
+
 styleFile <- system.file(package="RCPA", "extdata", "pieStyle.js")
 
 gs <- getGeneSets("KEGG")
@@ -15,8 +17,8 @@ results <- lapply(1:3, function(i) {
         name = gs$names,
         p.value = runif(length(genesets))/10,
         pFDR = runif(length(genesets))/10,
-        ES = runif(length(genesets)),
-        NES = runif(length(genesets)),
+        ES = rnorm(length(genesets)),
+        NES = rnorm(length(genesets)),
         nDE = sample(1:1000, length(genesets))
     )
 })
@@ -59,6 +61,15 @@ test_that('plot pathway network with edgeThreshold is 0', {
 
 test_that('plot pathway network with custom labels', {
     rcy <- plotPathwayNetwork(results, genesets, pThreshold = pThreshold, labels = gs$names[IDs])
+    expect_true("RCyjs" %in% class(rcy))
+    expect_true(rcy@port > 10000)
+    expect_true(rcy@port < 20000)
+    expect_true(all(rcy@graph@nodes %in% names(genesets)))
+    expect_true(all(names(genesets) %in% rcy@graph@nodes))
+})
+
+test_that('plot pathway network discrete mode', {
+    rcy <- plotPathwayNetwork(results, genesets, mode = "discrete")
     expect_true("RCyjs" %in% class(rcy))
     expect_true(rcy@port > 10000)
     expect_true(rcy@port < 20000)
